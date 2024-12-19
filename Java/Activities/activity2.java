1a5395ac5434752789834790f1acb658e14ef65d
Shweta20IBM <ssurvase@in.ibm.com>
2024-12-19 10:20:19 +0530

    Add files via upload

---
+++ Java/Activities/activity2.java
@@ -0,0 +1,17 @@
+
+public class activity2 {
+	public static void main(String[] args) {
+        // array with 6 numbers
+        int[] numbers = {10, 77, 10, 54, -11, 10};
+        
+        int sum = 0;
+        for (int num: numbers) {
+        	 if (num == 10) {
+                 sum += num;
+             }
+        	 
+        }
+        System.out.println(sum);
+           
+                   }
+}
