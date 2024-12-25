b9a726db811d26d01d7191c1a96166f56cb49958
Shweta20IBM <ssurvase@in.ibm.com>
2024-12-25 17:27:01 +0530

    Add files via upload

---
+++ Java/Activities/Activity8.java
@@ -0,0 +1,10 @@
+
+public class Activity8 {
+	  public static void main(String[] args) {
+	        try {
+	            throw new CustomException("This is a custom exception message.");
+	        } catch (CustomException e) {
+	            System.out.println(e.getMessage());
+	        }
+	    }
+}
