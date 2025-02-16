ec41f38318cbc969a809dc2fe24ae538c9b76d4e
Shweta20IBM <ssurvase@in.ibm.com>
2025-02-16 16:22:26 +0530

    Add files via upload

---
+++ Java/Activities/java_activity5.java
@@ -0,0 +1,29 @@
+abstract class Book{
+	String title;
+	abstract void setTitle(String s);
+	
+	String getTitle() 
+	{
+	return title;
+	}
+}
+
+class MyBook extends Book{
+	 public void setTitle(String s){
+		title =s;
+		}
+}
+public class java_activity5 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+MyBook newNovel= new MyBook();
+//Set title
+newNovel.setTitle("title of novel");
+
+//Print result
+System.out.println("The title is: " + newNovel.getTitle());
+	}
+
+
+		}
